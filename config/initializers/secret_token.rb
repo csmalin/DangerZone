7038bb1dd90702d593ca6f572ac5b07ac5c08d77
Chris Malin <csmalin@gmail.com>
2013-05-02 14:55:10 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dangerzone::Application.config.secret_token = '6cfeef8d2303b8f4bcf10ab476e2ae8368574686e964e4ffd97d6bcc5d203032cbefb4c794db18f772ac8ffa58afc829d1c2b1e27eede1d302d2dd1e1729a0d8'
