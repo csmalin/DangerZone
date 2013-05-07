db72d983d99186c6b90a08389f4e6c1779c59899
AlexRay <alex.james.ray@gmail.com>
2013-05-07 11:32:50 -0700

    threat_level bug

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -20,7 +20,7 @@ include ApplicationHelper
   def index
     #@crimes = Crime.near(params[:location], params[:distance].to_f)
     @crimes = Crime.all()
-    #@crimes.each_with_index {|crime, i| @crimes[i] = {latitude: crime.latitude, longitude: crime.longitude, safety_score: crime.threat_level}}
+    #@crimes.each_with_index {|crime, i| @crimes[i] = {latitude: crime.latitude, longitude: crime.longitude, threat_level: crime.threat_level}}
 
     @crimes.map!{|crime| crime.to_json}
     @crimes.map!{|crime| JSON.parse(crime)}
