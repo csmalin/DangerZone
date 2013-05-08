a4ca6fade51bdb7710af7e5952e08dea7e76df6f
deeptianand <anandeepti@gmail.com>
2013-05-08 12:16:19 -0700

    JS merge

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -6,16 +6,9 @@ class CrimesController < ApplicationController
   end
 
   def index
-    location_string = params[:location].split(",")
-    location_array = location_string.map{|x| x.to_f}
-    @crimes = Crime.near(location_array, params[:distance].to_f)
-    puts "location array #{location_array}"
-    puts "distance is #{params[:distance].to_f}"
-    puts "Crimes Count is #{@crimes.count}"
-    # @crimes = Crime.all()
-    #@crimes.each_with_index {|crime, i| @crimes[i] = {latitude: crime.latitude, longitude: crime.longitude, threat_level: crime.threat_level}}
-    # @crimes.map!{|crime| crime.to_json}
-    # @crimes.map!{|crime| JSON.parse(crime)}
+    @crimes = Crime.near(params[:location], params[:max_distance].to_f)
+    @crimes.keep_if{|crime| crime.distance.to_f >= params[:min_distance].to_f}
+    @crimes.each_with_index {|crime, i| @crimes[i] = {latitude: crime.latitude, longitude: crime.longitude, threat_level: crime.threat_level}}
 
     respond_to do |format|
       format.json { render :json => @crimes }
