26adfc64b034e6ad2873df0892dc758609be9190
AlexRay <alex.james.ray@gmail.com>
2013-05-07 19:25:38 -0700

    data load based off location and distance

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -18,9 +18,8 @@ include ApplicationHelper
   end
 
   def index
-    #@crimes = Crime.near(params[:location], params[:distance].to_f)
-    @crimes = Crime.all()
-    #@crimes.each_with_index {|crime, i| @crimes[i] = {latitude: crime.latitude, longitude: crime.longitude, threat_level: crime.threat_level}}
+    @crimes = Crime.near(params[:location], params[:distance].to_f)
+    @crimes.each_with_index {|crime, i| @crimes[i] = {latitude: crime.latitude, longitude: crime.longitude, threat_level: crime.threat_level}}
 
     @crimes.map!{|crime| crime.to_json}
     @crimes.map!{|crime| JSON.parse(crime)}
