d5bfe4150958293f289b013f07e25dba2bef5242
deeptianand <anandeepti@gmail.com>
2013-05-08 01:01:34 -0700

    Added Safety_score_logic

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -6,12 +6,16 @@ class CrimesController < ApplicationController
   end
 
   def index
-    #@crimes = Crime.near(params[:location], params[:distance].to_f)
-    @crimes = Crime.all()
+    location_string = params[:location].split(",")
+    location_array = location_string.map{|x| x.to_f}
+    @crimes = Crime.near(location_array, params[:distance].to_f)
+    puts "location array #{location_array}"
+    puts "distance is #{params[:distance].to_f}"
+    puts "Crimes Count is #{@crimes.count}"
+    # @crimes = Crime.all()
     #@crimes.each_with_index {|crime, i| @crimes[i] = {latitude: crime.latitude, longitude: crime.longitude, threat_level: crime.threat_level}}
-
-    @crimes.map!{|crime| crime.to_json}
-    @crimes.map!{|crime| JSON.parse(crime)}
+    # @crimes.map!{|crime| crime.to_json}
+    # @crimes.map!{|crime| JSON.parse(crime)}
 
     respond_to do |format|
       format.json { render :json => @crimes }
