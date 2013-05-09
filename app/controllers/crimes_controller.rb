2e0300fecc5d02cbac0d6c4e68fdb51eabce3610
AlexRay <alex.james.ray@gmail.com>
2013-05-09 08:55:19 -0700

    layer filter events

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -12,7 +12,10 @@ class CrimesController < ApplicationController
     @crimes = Crime.near(params[:location], params[:max_distance].to_f)
 
     @crimes.keep_if{|crime| crime.distance.to_f >= params[:min_distance].to_f}
-    @crimes.each_with_index {|crime, i| @crimes[i] = {latitude: crime.latitude, longitude: crime.longitude, threat_level: crime.threat_level}}
+    @crimes.each_with_index {|crime, i| @crimes[i] = {latitude: crime.latitude,
+                                                      longitude: crime.longitude,
+                                                      threat_level: crime.threat_level,
+                                                      category: crime.category}}
 
     respond_to do |format|
       format.json { render :json => @crimes }
